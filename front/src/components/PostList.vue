92ffbde1bcf57dd860e045cc71db7924220a950d
joonanyl <jjoonanylander@gmail.com>
2021-12-14 18:05:29 +0200

    rm comments

--- front/src/components/PostList.vue
+++ front/src/components/PostList.vue
@@ -29,36 +29,3 @@ export default {
   },
 };
 </script>
-
-<!--
-posts: [
-        {
-          title: "First post!",
-          writer: "Joona",
-          date: "07.12.2021",
-          emotion: "happy",
-          content: "This is my first blog post!",
-        },
-        {
-            title: "Second post",
-            writer: "Joona",
-            date: "08.12.2021",
-            emotion: "surprised",
-            content: "This is my second blog post!",
-        },
-        {
-            title: "Post three",
-            writer: "Joona",
-            date: "10.12.2021",
-            emotion: "angry",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        },
-        {
-            title: "Post four",
-            writer: "Joona",
-            date: "10.12.2021",
-            emotion: "sad",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        }
-      ],
--->
